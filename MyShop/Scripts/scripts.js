d1dc3dd2397e654c32d7df3c820c08965ea9b659
Maciej_win <Maciej_win@DESKTOP-RBV81ND>
2020-01-29 15:24:47 +0100

    css fixes, js refactoring

--- MyShop/Scripts/scripts.js
+++ MyShop/Scripts/scripts.js
@@ -23,6 +23,46 @@
         window.location.replace("/Product/AddProduct/");
     }
 
-    
+    function UpdateTotalPrice() {
+        $.ajax({
+            type: 'GET',
+            url: "Cart/UpdateTotal",
+            contentType: "application/json; charset=utf-8",
+            success: function (msg) {
+                if (msg.d == "$0.00" || msg.d === "" || String.length == 0 || msg == null) {
+                    //$('#purchaseButton').hide();
+                    location.reload();
+                    return;
+                }
+                $('#totalPrice').text(msg.d);
+                $('#cart-total').text(msg.d);
+            }
+        })
+    }
+    function Change(el, type, pid) {
+        var data = {
+            'type': type,
+            'pId': pid
+        };
+        $.ajax({
+            type: 'POST',
+            url: "Cart/QuanityChange",
+            data: "{ 'type': " + type + ", 'pId': " + pid + "}",
+            contentType: "application/json; charset=utf-8",
+            success: function (msg) {
+                if (msg.d == 0) {
+                    el.parentNode.parentNode.remove();
+                    //$('#purchaseButton').hide()
+                } else {
+                    $(el).siblings('span')[0].innerHTML = msg.d
+                }
+                UpdateTotalPrice()
+            },
+            error: function (XMLHttpRequest, textStatus, errorThrown) {
+                debugger;
+            }
+        });
+
+    }
 
     
